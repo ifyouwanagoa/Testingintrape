ad3f13bc2b90f3766d63af2c66a290ca6eec97d5
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2019-04-02 21:58:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,4 +29,15 @@ client.on('message', message => {
   }
   });
 
+client.on('message') , msg => {
+              var prefix = "$" ;
+   if(msg.content.startsWith(prefix + "msgcount")) {
+msg.channel.fetchMessage()
+  .then(messages => msg.channel.send(`Message Count
+${messages.size} `))
+  .catch(console.error);
+  }
+  })
+
+
 client.login(process.env.BOT_TOKEN);
