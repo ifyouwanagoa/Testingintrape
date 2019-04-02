d65db3ee2eb1b08217e0f1c77e9b9ad21de0bd0c
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2019-04-02 21:45:17 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+// playing
+client.on('ready', () => {                           
+});
+
+
+
+ 
+
+client.login(process.env.BOT_TOKEN);
