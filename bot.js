70c6b60e03ff5eb3827dbeecbd3bb8c9bfc676df
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2019-04-02 21:52:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,11 @@ const client = new Discord.Client();
 client.on('ready', () => {                           
 });
 
-client.on('message') , msg => {
-              var prefix = "$" ;
-   if(msg.content.startsWith(prefix + "msgcount")) {
-msg.channel.fetchMessage()
-  .then(messages => msg.channel.send(`Message Count
-${messages.size} `))
-  .catch(console.error);
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
-  })
+});
 
 client.login(process.env.BOT_TOKEN);
