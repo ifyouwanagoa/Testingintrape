39ac5e94336e3ea8a2180feec7bce37fccea4977
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2019-04-02 21:50:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,14 @@ const client = new Discord.Client();
 client.on('ready', () => {                           
 });
 
-
-
- 
+client.on('message') , msg => {
+              var prefix = "$" ;
+   if(msg.content.startsWith(prefix + "msgcount")) {
+msg.channel.fetchMessage()
+  .then(messages => msg.channel.send(`Message Count
+${messages.size} `))
+  .catch(console.error);
+  }
+  })
 
 client.login(process.env.BOT_TOKEN);
